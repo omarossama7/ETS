2ad4aa0629337d02fd45576b6a2efc31b050f2dc
Omasr12 <134226274+Omasr12@users.noreply.github.com>
2025-04-21 19:57:29 +0200

    added event routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
+const eventRoutes = require('./routes/eventRoutes')
 const connectDB = require('./config/db');
 const app = express();
 
@@ -11,6 +12,7 @@ connectDB();
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/api/v1', eventRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
